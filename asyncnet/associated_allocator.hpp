fc4414efe71f912d186e681a20fc4a2fce126e3a
chingyat <tsingyat@outlook.com>
2020-11-08 00:36:29 +0800

    add detection for nested allocator_type and executor_type

--- asyncnet/associated_allocator.hpp
+++ asyncnet/associated_allocator.hpp
@@ -5,9 +5,11 @@
 #ifndef ASYNCNET_ASSOCIATED_ALLOCATOR_HPP
 #define ASYNCNET_ASSOCIATED_ALLOCATOR_HPP
 
+#include <type_traits>
+
 namespace asyncnet {
 
-template<typename T, typename Allocator = std::allocator<void>>
+template <typename T, typename Allocator = std::allocator<void>, typename = std::void_t<>>
 struct associated_allocator
 {
   using type = Allocator;
@@ -16,18 +18,33 @@ struct associated_allocator
   {
     (void) t;
     return a;
+  };
+};
+
+template <typename T, typename Allocator>
+struct associated_allocator<T,
+                            Allocator,
+                            std::void_t<typename T::allocator_type, decltype(std::declval<const T &>().get_allocator())>>
+{
+  using type = typename T::allocator_type;
+
+  static type get(const T &t, const Allocator &a = Allocator())
+  {
+    (void) a;
+    return t.get_allocator();
   }
 };
 
-template<typename T, typename Allocator = std::allocator<void>>
+
+template <typename T, typename Allocator = std::allocator<void>>
 using associated_allocator_t = typename associated_allocator<T, Allocator>::type;
 
-template<typename T, typename Allocator = std::allocator<void>>
+template <typename T, typename Allocator = std::allocator<void>>
 associated_allocator_t<T, Allocator> get_associated_allocator(const T &t, const Allocator &a = Allocator())
 {
   return associated_allocator<T, Allocator>::get(t, a);
 }
 
-}
+} // namespace asyncnet
 
 #endif //ASYNCNET_ASSOCIATED_ALLOCATOR_HPP
--- asyncnet/associated_executor.hpp
+++ asyncnet/associated_executor.hpp
@@ -7,29 +7,42 @@
 
 #include <asyncnet/system_executor.hpp>
 
+#include <type_traits>
+
 namespace asyncnet {
 
-template<typename T, typename Executor = system_executor>
+template <typename T, typename Executor = system_executor, typename = std::void_t<>>
 struct associated_executor
 {
   using type = Executor;
 
-  static type get(const T &t, const type &ex = Executor())
+  static type get(const T &t, const Executor &ex = Executor())
   {
     (void) t;
     return ex;
   }
 };
 
-template<typename T, typename Executor = system_executor>
+template <typename T, typename Executor>
+struct associated_executor<T, Executor, std::void_t<typename T::executor_type, decltype(std::declval<const T &>().get_executor())>>
+{
+  using type = typename T::executor_type;
+
+  static type get(const T &x, const Executor & = Executor())
+  {
+    return x.get_executor();
+  }
+};
+
+template <typename T, typename Executor = system_executor>
 using associated_executor_t = typename associated_executor<T, Executor>::type;
 
-template<typename T, typename Executor = system_executor>
+template <typename T, typename Executor = system_executor>
 associated_executor_t<T, Executor> get_associated_executor(const T &t, const Executor &ex = Executor())
 {
   return associated_executor<T, Executor>::get(t, ex);
 }
 
-}
+} // namespace asyncnet
 
 #endif //ASYNCNET_ASSOCIATED_EXECUTOR_HPP
