95695b6a6972871bf19887f8f6f2b164f6d19280
chingyat <tsingyat@outlook.com>
2020-11-08 00:37:27 +0800

    add some document comments

--- asyncnet/async_completion.hpp
+++ asyncnet/async_completion.hpp
@@ -10,14 +10,20 @@
 
 namespace asyncnet {
 
+/// A convenient class that helps to obtain completion handlers and async_result objects
+/// from completion tokens.
+///
+/// @tparam CompletionToken Completion token type.
+/// @tparam Signature Signature of the completion handler.
 template <typename CompletionToken, typename Signature>
 struct async_completion
 {
   using completion_handler_type = typename async_result<
       std::decay_t<CompletionToken>, Signature>::completion_handler_type;
 
-  mutable completion_handler_type completion_handler;
-  async_result <std::decay_t<CompletionToken>, Signature> result;
+  completion_handler_type completion_handler;
+
+  async_result<std::decay_t<CompletionToken>, Signature> result;
 
   explicit async_completion(CompletionToken &token)
       : completion_handler(std::forward<CompletionToken>(token)),
@@ -26,6 +32,6 @@ struct async_completion
   }
 };
 
-}
+} // namespace asyncnet
 
 #endif //ASYNCNET_ASYNC_COMPLETION_HPP
--- asyncnet/detail/config.hpp
+++ asyncnet/detail/config.hpp
@@ -1,12 +1,65 @@
 //
-// Created by lince on 11/7/20.
+// config.hpp
+// ~~~~~~~~~~
+//
+// Copyright 2020 Lince
 //
 
 #ifndef ASYNCNET_DETAIL_CONFIG_HPP
 #define ASYNCNET_DETAIL_CONFIG_HPP
 
-#define ASYNCNET_DECL inline
+#if !defined(ASYNCNET_HEADER_ONLY) & !defined(ASYNCNET_STANDARDESE)
+#  if !defined(ASYNCNET_SEPARATE_COMPILATION)
+#    if !defined(ASYNCNET_DYN_LINK)
+#      define ASYNCNET_HEADER_ONLY 1
+#    endif
+#  endif
+#endif
 
-#define ASYNCNET_HEADER_ONLY
+#if defined(ASYNCNET_HEADER_ONLY)
+#  define ASYNCNET_DECL inline
+#elif defined(ASYNCNET_DYN_LINK)
+#  if defined(_MSC_VER)
+#    if defined(ASYNCNET_SOURCE)
+#      define ASYNCNET_DECL __declspec(dllexport)
+#    else
+#      define ASYNCNET_DECL __declspec(dllimport)
+#    endif
+#  elif defined(__GNUC__)
+#    define ASYNCNET_DECL __attribute__((visibility("default")))
+#  else
+#    define ASYNCNET_DECL
+#  endif
+#else
+#  define ASYNCNET_DECL
+#endif
 
-#endif //ASYNCNET_DETAIL_CONFIG_HPP
+#ifndef ASYNCNET_DECL_INLINE
+#  if defined(__GNUC__) && !defined(_WIN32)
+#    define ASYNCNET_DECL_INLINE __always_inline
+#  elif defined(_MSC_VER) || defined(_WIN32)
+#    define ASYNCNET_DECL_INLINE __forceinline
+#  else
+#    define ASYNCNET_DECL_INLINE inline
+#  endif
+#endif
+
+#ifdef _WIN32
+#  define ASYNCNET_WINDOWS
+#endif
+
+#ifdef __COBALT__
+#  define ASYNCNET_XENOMAI
+#endif
+
+#if !defined(_WIN32) || defined(__COBALT__)
+#  define ASYNCNET_POSIX
+#endif
+
+#ifdef ASYNCNET_WINDOWS
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
+#endif
+
+#endif
